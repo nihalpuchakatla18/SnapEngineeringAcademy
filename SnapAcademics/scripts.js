febf4ab6cb9b77c2bfdb9a01fe190301fcdba0dd
Nihal Puchakatla <132979773+nihalpuchakatla18@users.noreply.github.com>
2025-04-13 14:00:59 -0700

    Update scripts.js

--- SnapAcademics/scripts.js
+++ SnapAcademics/scripts.js
@@ -23,20 +23,6 @@
  *
  */
 
-const FRESH_PRINCE_URL =
-  "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-  "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
-
-// Array of TV show titles
-let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High"
-];
-
 // Array to store all games
 let games = [];
 let filteredGames = [];
